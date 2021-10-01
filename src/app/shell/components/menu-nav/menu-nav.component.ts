c5eedb83fa9b2361843aae3873bb71e95ba3662d
Adenilson Souza Santos Junior - AJN <adenilson.souza@iteris.local>
2021-10-01 09:40:44 -0300

    chore: ajustando sass

--- src/app/shell/components/menu-nav/menu-nav.component.ts
+++ src/app/shell/components/menu-nav/menu-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-nav',
   templateUrl: './menu-nav.component.html',
-  styleUrls: ['./menu-nav.component.css']
+  styleUrls: ['./menu-nav.component.scss']
 })
 export class MenuNavComponent implements OnInit {
 
--- src/app/shell/components/nav-rigth/nav-rigth.component.ts
+++ src/app/shell/components/nav-rigth/nav-rigth.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-rigth',
   templateUrl: './nav-rigth.component.html',
-  styleUrls: ['./nav-rigth.component.css']
+  styleUrls: ['./nav-rigth.component.scss']
 })
 export class NavRigthComponent implements OnInit {
 
